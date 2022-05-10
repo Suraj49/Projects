91c594461bb2ab66531922ca853660649ecd4168
Suraj49 <1507surajkumar@gmail.com>
2022-05-10 18:43:47 +0530

    commits

---
+++ File System Organizer/File System/FSO.js
@@ -0,0 +1,14 @@
+ let input = process.argv.slice(2)
+
+ console.log(input)
+
+//  switch(command){
+//      case 'tree' :
+//          console.log('Tree Implemented')
+
+//      case 'organize' :
+//          console.log('Organize Implemented')   
+         
+//      case 'help' :
+//          console.log('Help Implemented')
+//  } 
